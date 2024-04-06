6927bfc2afd4c1ed53c1d3fbb55a6c5422d5cac1
Guany <1084072690@qq.com>
2024-04-06 12:56:59 +0800

    feat: initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# nuxt-starter
+
+🚀 Nuxt starter
+
+## 使用说明
+
+```sh
+npx degit tlyboy/nuxt-starter my-new-project
+```
+
+## 使用许可
+
+[MIT](https://opensource.org/licenses/MIT) © Guany
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['~/assets/styles/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: [
+    '@vue-macros/nuxt',
+    '@nuxtjs/color-mode',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@vueuse/nuxt',
+  ],
+  colorMode: {
+    classSuffix: '',
+  },
+  imports: {
+    dirs: ['stores'],
+  },
+})
